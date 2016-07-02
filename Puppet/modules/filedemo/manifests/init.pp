246726dd6c0e5042d6f5d54132182e4e22f69bf0
yuvatheja <yuvatheja.s@gmail.com>
2016-07-01 20:09:41 -0500

    'filedemov2'

--- Puppet/modules/filedemo/manifests/init.pp
+++ Puppet/modules/filedemo/manifests/init.pp
@@ -1,7 +1,26 @@
 class filedemo{
+		File{
+			owner => 'root',
+			group => 'finance',
+			mode => '0660',
+		}
+		$homedir="/root"
+		file{"${homedir}/myfile.txt":		
+			content => 'myfile content',
+		}
+		file{"${homedir}/myfile2.txt":
+			content => "my file content 2",
+		}
+		file{"${homedir}/myfile3.txt":
+			content => "my file content 3",
+			owner	=> admin,
+			group	=> root,
+		}
+		
 		file{'/root/motd':			
 			ensure 	=> present,		
-			source 	=> 'puppet:///modules/filedomo/motd',  root,
+			source 	=> 'puppet:///modules/filedomo/motd',
+			owner 	=> root,
 			group 	=> root,
 			mode	=>'0644',
 		}
