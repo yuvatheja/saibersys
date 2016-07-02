b181ac49e6aacc4245b5c3edb06e9697fc5ceb19
yuvatheja <yuvatheja.s@gmail.com>
2016-07-01 19:34:38 -0500

    'filedemo1'

--- Puppet/modules/filedemo/manifests/init.pp
+++ Puppet/modules/filedemo/manifests/init.pp
@@ -1,8 +1,7 @@
 class filedemo{
 		file{'/root/motd':			
 			ensure 	=> present,		
-			content => 'this is my motd file managed by content attribute', 
-			owner 	=> root,
+			source 	=> 'puppet:///modules/filedomo/motd',  root,
 			group 	=> root,
 			mode	=>'0644',
 		}
