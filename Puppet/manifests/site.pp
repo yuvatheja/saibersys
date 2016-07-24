250b6564d200c1006c3550dbe15f41b6da8bee5e
yuvatheja <yuvatheja.s@gmail.com>
2016-07-24 12:05:16 -0500

    'apache-v2'

--- Puppet/manifests/site.pp
+++ Puppet/manifests/site.pp
@@ -2,5 +2,9 @@ node "nodehostname"{
 		include localusers
 		include localusers::groups::wheel
 		include localusers::groups::finance
+		include base
+		include apache
+		include filedemo
+		include ntp
 		notify{'this is a test notify':}
 	}
